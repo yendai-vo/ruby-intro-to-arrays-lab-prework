c629113f51fe1eaa53a9b5d5d0f2b4ec5b523abd
Cindy Vo <yendai.vo@hotmail.com>
2018-07-10 21:52:55 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -7,7 +7,7 @@ def array_with_two_elements
 end
 
 def first_element(array)
-  array.first
+  array[0]
 end
 
 def third_element(array)
@@ -20,4 +20,12 @@ end
 
 def length_of_array(array)
   array.count
+end
+
+def first_element_with_array_methods(array)
+  array.first
+end
+
+def last_element_with_array_methods(array)
+  array.last
 end
